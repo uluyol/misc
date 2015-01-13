958b881ea9269e414debd61cbac19e196294b5d6
Muhammed Uluyol <uluyo002@umn.edu>
2015-01-13 06:11:36 -0600

    More idiomatic C++

--- bloom.cc
+++ bloom.cc
@@ -15,7 +15,7 @@ public:
 	size_t length();
 	void set(size_t k);
 	bool get(size_t k);
-	std::string *to_string();
+	std::string to_string();
 };
 
 Bitstring::Bitstring(size_t siz) {
@@ -42,12 +42,12 @@ bool Bitstring::get(size_t k) {
 	return (bytes[pos] >> bit) & 1;
 }
 
-std::string *Bitstring::to_string() {
-	std::string *s = new std::string(sizeof(uint8_t) * siz, '0');
+std::string Bitstring::to_string() {
+	std::string s(sizeof(uint8_t) * siz, '0');
 	for (int i = 0; i < siz; i++) {
 		for (int j = 0; j < sizeof(uint8_t); j++) {
 			size_t bit = i * sizeof(uint8_t) + j;
-			(*s)[bit] = '0' + (char)this->get(bit);
+			s[bit] = '0' + (char)this->get(bit);
 		}
 	}
 	return s;
@@ -57,68 +57,62 @@ std::string *Bitstring::to_string() {
 // one at the moment
 template <typename T>
 class BloomFilter {
-	Bitstring *bits;
-	std::vector<size_t> *make_hashes(T obj);
+	Bitstring bits;
+	void make_hashes(T obj, std::vector<size_t> &hashes);
 public:
 	BloomFilter(size_t siz);
-	~BloomFilter();
 	void add(T obj);
 	bool has(T obj);
-	std::string *to_string();
+	std::string to_string();
 };
 
 template <typename T>
-BloomFilter<T>::BloomFilter(size_t siz) { bits = new Bitstring(siz); }
+BloomFilter<T>::BloomFilter(size_t siz) : bits(siz) {}
 
 template <typename T>
-BloomFilter<T>::~BloomFilter() { delete bits; }
-
-template <typename T>
-std::vector<size_t> *BloomFilter<T>::make_hashes(T obj) {
+void BloomFilter<T>::make_hashes(T obj, std::vector<size_t> &hashes) {
 	size_t shift_amount = sizeof(size_t) / 2;
-	auto hashes = new std::vector<size_t>;
 	std::hash<T> hfunc;
 	size_t hash = hfunc(obj);
-	hashes->push_back((hash << shift_amount) >> shift_amount);
-	hashes->push_back(hash >> shift_amount);
-	return hashes;
+	hashes.push_back((hash << shift_amount) >> shift_amount);
+	hashes.push_back(hash >> shift_amount);
 }
 
 template <typename T>
 void BloomFilter<T>::add(T obj) {
-	std::vector<size_t> *hashes = make_hashes(obj);
-	for (auto &hash : *hashes) {
-		bits->set(hash % bits->length());
+	std::vector<size_t> hashes;
+	make_hashes(obj, hashes);
+	for (auto &hash : hashes) {
+		bits.set(hash % bits.length());
 	}
-	delete hashes;
 }
 
 template <typename T>
 bool BloomFilter<T>::has(T obj) {
-	std::vector<size_t> *hashes = make_hashes(obj);
+	std::vector<size_t> hashes;
+	make_hashes(obj, hashes);
 	bool has = true;
-	for (auto &hash : *hashes) {
-		has &= bits->get(hash % bits->length());
+	for (auto &hash : hashes) {
+		has &= bits.get(hash % bits.length());
 	}
-	delete hashes;
 	return has;
 }
 
 template <typename T>
-std::string *BloomFilter<T>::to_string() {
-	return bits->to_string();
+std::string BloomFilter<T>::to_string() {
+	return bits.to_string();
 }
 
 int main(int argc, const char **argv) {
 	// Ignore memory leaks here
-	auto bf = new BloomFilter<std::string>(10);
-	std::cout << *bf->to_string() << std::endl;
-	bf->add("Happy");
-	std::cout << *bf->to_string() << std::endl;
-	bf->add("Sad");
-	std::cout << *bf->to_string() << std::endl;
-	std::cout << "Contains: \"Happy\": " << bf->has("Happy") << std::endl;
-	std::cout << "Contains: \"Sad\": " << bf->has("Sad") << std::endl;
-	std::cout << "Contains: \"Not sad\": " << bf->has("Not sad") << std::endl;
+	BloomFilter<std::string> bf(10);
+	std::cout << bf.to_string() << std::endl;
+	bf.add("Happy");
+	std::cout << bf.to_string() << std::endl;
+	bf.add("Sad");
+	std::cout << bf.to_string() << std::endl;
+	std::cout << "Contains: \"Happy\": " << bf.has("Happy") << std::endl;
+	std::cout << "Contains: \"Sad\": " << bf.has("Sad") << std::endl;
+	std::cout << "Contains: \"Not sad\": " << bf.has("Not sad") << std::endl;
 	return 0;
 }
